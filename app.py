a096704412641fe54bdecce9c392e7ba12deb737
aulaopet <darkyymeiacinco@gmail.com>
2026-05-14 20:52:25 -0300

    mais coisa

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, Response
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return Response("OK AZURE", status=200)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)  
+
+
+
+
+
+
+
+
