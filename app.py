24257ff3cf56c58b8028a879bd568e9cb5c22e90
aulaopet <darkyymeiacinco@gmail.com>
2026-05-21 21:04:06 -0300

    aaaa

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return Response("OK AZURE", status=200)
+    return Response("OK AZURE 2", status=200)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
